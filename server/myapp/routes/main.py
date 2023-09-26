02de8ddbfe5f56ff9567dce03850dca70267cf7c
Raymond Dang <raymondquocdang@gmail.com>
2023-09-25 23:04:20 -0400

    update

--- server/myapp/routes/main.py
+++ server/myapp/routes/main.py
@@ -28,9 +28,9 @@ def catch_all(path):
     return send_from_directory(app.static_folder, "index.html")
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+# app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 # db = SQLAlchemy(app)
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+# app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 logging.basicConfig(
