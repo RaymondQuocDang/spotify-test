ee815c70a42d05531def039e1a8c030d836229d1
Raymond Dang <raymondquocdang@gmail.com>
2023-09-25 22:28:33 -0400

    update sql

--- server/myapp/routes/main.py
+++ server/myapp/routes/main.py
@@ -60,7 +60,6 @@ create_table = ''' CREATE TABLE IF NOT EXISTS public.newtbl(
     email character varying(120) COLLATE pg_catalog."default" NOT NULL,
     userName character varying(80) COLLATE pg_catalog."default" NOT NULL,
     password character varying(120) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT newtbl_pkey PRIMARY KEY (id),
     CONSTRAINT newtbl_email_key UNIQUE (email),
     CONSTRAINT newtbl_username_key UNIQUE (username)); '''
 
